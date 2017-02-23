dd3622f3f0198d7f505332970472637f30f8e56b
Theo Gantzer <theogantzer@Fronts-MacBook-Pro-2.local>
2017-02-23 12:49:47 -0800

    cleaning comments

--- server.js
+++ server.js
@@ -4,11 +4,5 @@ var app = express();
 
 app.use(express.static('public'));
 
-
-// app.get('/', function(req, res){
-//   res.render('template', { company: process.env.COMPANY_NAME, strikeprice: process.env.STRIKE_PRICE, nbofshares: process.env.NB_FULLY_DILUTED_SHARES });
-// });
-
-
 var port = process.env.PORT || 2676;
 app.listen(port, function () { console.log('App running on port', port); });
